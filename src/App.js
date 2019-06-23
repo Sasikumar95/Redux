812bfefe5b32b878a98ec3bc75d86b9479da4e81
SasikumarC95 <sasimymails@gmail.com>
2019-06-23 17:21:29 +0530

    learn redux

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+      <div className="App">
+       <h1>Hello World</h1>
+       <h1>{React.version}</h1>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+// import loggedReducer from "./islogged";
+// import conuterReducer from "./counter";
+// import {combineReducers} from "redux"
+
+
+// const allReducers= combineReducers({
+//     counter:conuterReducer,
+//     islogged:loggedReducer
+// });
+
+// export default allReducers;
